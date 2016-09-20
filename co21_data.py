5c9124c463a59b372f75b7280995b3bf07afdda7
Yanitski <yanitski@ualberta.ca>
2016-09-20 22:24:44 +0000

    I have rearranged the axes, added canonical trends according to the Milky Way, and fixed a slight issue of the size of the data points

--- co21_data.py
+++ co21_data.py
@@ -2,28 +2,28 @@ import mkgraph
 
 #NGC1672
 # Load the data file and display it in a few graphs.
-mkgraph.graphs ('NGC1672_co21_cube_props_clfind.fits','NGC1672.png')
+mkgraph.graphs ('NGC 1672','NGC1672_co21_cube_props_clfind.fits','NGC1672.png')
 
 #NGC3351
-mkgraph.graphs ('NGC3351_co21_cube_props_clfind.fits','NGC3351.png')
+mkgraph.graphs ('NGC 3351','NGC3351_co21_cube_props_clfind.fits','NGC3351.png')
 
 #NGC3627
-mkgraph.graphs ('NGC3627_co21_cube_props_clfind.fits','NGC3627.png')
+mkgraph.graphs ('NGC 3627','NGC3627_co21_cube_props_clfind.fits','NGC3627.png')
 
 #NGC4254
-mkgraph.graphs ('NGC4254_co21_cube_props_clfind.fits','NGC4254.png')
+mkgraph.graphs ('NGC 4254','NGC4254_co21_cube_props_clfind.fits','NGC4254.png')
 
 #NGC4303
-mkgraph.graphs ('NGC4303_co21_cube_props_clfind.fits','NGC4303.png')
+mkgraph.graphs ('NGC 4303','NGC4303_co21_cube_props_clfind.fits','NGC4303.png')
 
 #NGC4321
-mkgraph.graphs ('NGC4321_co21_cube_props_clfind.fits','NGC4321.png')
+mkgraph.graphs ('NGC 4321','NGC4321_co21_cube_props_clfind.fits','NGC4321.png')
 
 #NGC4535
-mkgraph.graphs ('NGC4535_co21_cube_props_clfind.fits','NGC4535.png')
+mkgraph.graphs ('NGC 4535','NGC4535_co21_cube_props_clfind.fits','NGC4535.png')
 
 #NGC5068
-mkgraph.graphs ('NGC5068_co21_cube_props_clfind.fits','NGC5068.png')
+mkgraph.graphs ('NGC 5068','NGC5068_co21_cube_props_clfind.fits','NGC5068.png')
 
 #NGC6744
-mkgraph.graphs ('NGC6744_co21_cube_props_clfind.fits','NGC6744.png')
+mkgraph.graphs ('NGC 6744','NGC6744_co21_cube_props_clfind.fits','NGC6744.png')
--- mkgraph.py
+++ mkgraph.py
@@ -1,26 +1,61 @@
-# This is a module to display data in a series of graphs
-def graphs(data,ofile):
+# This is a module to save data in a series of graphs.
+def graphs (galaxy,data,ofile):
+
+        # import the libraries that are needed.
 	from astropy.table import Table
 	import matplotlib.pyplot as plt
+
+	# Load all of the data.
 	mytable = Table.read(data)
+
+	# Extract different data sets.
+	vmass = mytable['VIRMASS_EXTRAP_DECONV']
+	lmass = mytable['MASS_EXTRAP']
+	radius = mytable['RADRMS_EXTRAP_DECONV']
+	lw = mytable['VRMS_EXTRAP']
+
+	# Calculate the canonical values according to the Milky Way.
+	mcan = 170*radius**2
+	lwcan = 0.7*radius**0.5
+
+	# Initialize a figure that can easily show three plots
+        #arranged horizontally.
 	figure = plt.figure(figsize=(16,6))
-	# Create a subplot for the GMC virial and luminous masses
+
+	# Create a subplot for the GMC luminous and virial masses.
 	plt.subplot(1,3,1)
-	plt.loglog(mytable['VIRMASS_EXTRAP_DECONV'],mytable['MASS_EXTRAP'],marker='s',linestyle='None')
-	plt.xlabel(r'$M_{\mathrm{vir}}\ (M_{\odot})$')
-	plt.ylabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$') 
-	plt.title('Virial Mass v Luminous Mass')
-	# Create a subplot for the GMC luminous mass and radius
+	lines = plt.loglog (lmass,vmass,lmass,lmass)
+	l1,l2 = lines
+	plt.setp (l1,marker='s',markersize=5,linestyle='None')
+	plt.setp (l2,color='k',linestyle='solid',linewidth=3,alpha=0.5)
+	plt.legend ([l1,l2],[galaxy,r'$M_{\mathrm{lum}}$'],loc=2)
+	plt.xlabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$')
+	plt.ylabel(r'$M_{\mathrm{vir}}\ (M_{\odot})$')
+	plt.title('Luminous Mass vs. Virial Mass for GMCs in '+galaxy)
+
+	# Create a subplot for the GMC luminous radius and mass.
 	plt.subplot(1,3,2)
-	plt.loglog(mytable['MASS_EXTRAP'],mytable['RADRMS_EXTRAP_DECONV'],marker='s',linestyle='None')
-	plt.xlabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$') 
-	plt.ylabel(r'$\mathrm{Radius}\ (\mathrm{pc})$')
-	plt.title('Luminous Mass v Radius')
-	# Create a subplot for the GMC linewidth and radius
+	lines = plt.loglog(radius,lmass,radius,mcan)
+	l1,l2 = lines
+	plt.setp(l1,marker='s',markersize=5,linestyle='None')
+	plt.setp(l2,color='k',linestyle='solid',linewidth=3,alpha=0.5)
+	plt.legend ([l1,l2],[galaxy,'Canonical Mass'],loc=2)
+	plt.xlabel(r'$\mathrm{Radius}\ (\mathrm{pc})$')
+	plt.ylabel(r'$M_{\mathrm{lum}}\ (M_{\odot})$')
+	plt.title('Radius vs. Luminous Mass for GMCs in '+galaxy)
+
+	# Create a subplot for the GMC radius and linewidth.
 	plt.subplot(1,3,3)
-	plt.loglog(mytable['VRMS_EXTRAP'],mytable['RADRMS_EXTRAP_DECONV'],marker='s',linestyle='None')
-	plt.xlabel(r'$\mathrm{Linewidth}\ (\frac{km}{s})$') 
-	plt.ylabel(r'$\mathrm{Radius}\ (\mathrm{pc})$')
-	plt.title('Linewidth v Radius')
-	plt.tight_layout() 	
+	lines = plt.loglog(radius,lw,radius,lwcan)
+        l1,l2 = lines
+	plt.setp(l1,marker='s',markersize=5,linestyle='None')
+	plt.setp(l2,color='k',linestyle='solid',linewidth=3,alpha=0.5)
+	plt.legend ([l1,l2],[galaxy,'Canonical Linewidth'],loc=2)
+	plt.xlabel(r'$\mathrm{Radius}\ (\mathrm{pc})$')
+	plt.ylabel(r'$\mathrm{Linewidth}\ (\mathrm{km})$')
+	plt.title('Radius vs. Linewidth for GMCs in '+galaxy)
+
+
+	plt.tight_layout()
 	plt.savefig(ofile)
+
